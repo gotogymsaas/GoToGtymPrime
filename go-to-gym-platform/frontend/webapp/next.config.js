b595b031426e023a1abde106b72b903680e62b59
Go To Gym Sportswear as a Service <84112447+gotogymsaas@users.noreply.github.com>
2025-06-10 22:03:25 -0500

    Add i18n configuration and components

--- go-to-gym-platform/frontend/webapp/next.config.js
+++ go-to-gym-platform/frontend/webapp/next.config.js
@@ -2,7 +2,9 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
 });
+const { i18n } = require('./next-i18next.config.js');
 
 module.exports = withPWA({
   reactStrictMode: true,
+  i18n,
 });
