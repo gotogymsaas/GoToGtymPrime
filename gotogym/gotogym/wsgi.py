18ed4f77177f8a20cbc12723972bdc6b95c0bdda
Alvaro272915 <alvarourregoviana@gmail.com>
2025-08-06 15:47:59 -0500

    Ajuste para la configuracion de azure

--- gotogym/gotogym/wsgi.py
+++ gotogym/gotogym/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gotogym.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gotogym.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wellness_monitor.settings')
 
 application = get_wsgi_application()
