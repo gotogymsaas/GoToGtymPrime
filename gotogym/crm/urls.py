16bdd0edff6840b976eb5ceb651c3e871989c164
Alvaro272915 <alvarourregoviana@gmail.com>
2025-08-14 09:42:30 -0500

    Para despliegue en Azure

--- gotogym/crm/urls.py
+++ gotogym/crm/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
-from . import views
 
-urlpatterns = [
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
     # Aquí puedes agregar endpoints para sincronizar o consultar datos de HubSpot
-    path('contacts/', views.hubspot_contacts, name='hubspot_contacts'),
+#     path('contacts/', views.hubspot_contacts, name='hubspot_contacts'),
+# ]
+
+from django.http import HttpResponse
+from django.urls import path
+def healthz(request):
+    return HttpResponse("OK")
+urlpatterns = [
+    # ...
+    path("healthz", healthz),
 ]
